bf734d4178bbf96258523ecbfa83ad000119315f
icchyr <icchyr@gmail.com>
2017-03-05 09:48:07 +0900

    文字コード，順位を修正

--- invisible-sakura/background.js
+++ invisible-sakura/background.js
@@ -2,12 +2,23 @@ chrome.webRequest.onBeforeRequest.addListener( function (d) {
     var xhr = new XMLHttpRequest();
     xhr.open(d.method, d.url, false);
     xhr.send(null);
-    var json = JSON.parse(xhr.responseText);
-    var res = json.filter(function(el) {
+    var rank_sakura;
+    var json = JSON.parse(xhr.responseText).filter(function(el) {
+      if (el.team && el.team.name == "さくら") {
+        rank_sakura = el.rank;
+      }
       return el.team && el.team.name != "さくら";
     });
+    var res = [];
+    var r = 1;
+    json.forEach(function (el) {
+      if (el.rank > rank_sakura) {
+        el.rank--;
+      }
+      res.push(el);
+    });
     return {
-      redirectUrl: "data:application/json,"+JSON.stringify(res)
+      redirectUrl: "data:application/json; charset=utf-8,"+JSON.stringify(res)
     };
   },{
     urls: ["http://contest.ictsc/api/scoreboard"]
